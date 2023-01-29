061405fae10aa1d64afff82681a01b49894e5b66
Lyudmila Kostanyan <lykostan@c2r8s4.42yerevan.am>
2023-01-29 20:29:23 +0400

    gnl started

---
+++ gnl/get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 5
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+char	*ft_strdup(const char *s, int j);
+int		ft_strchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+
+#endif
